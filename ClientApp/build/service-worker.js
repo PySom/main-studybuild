4a89e6742515ed2709c4ccab0494f9b964826b55
Nwisu Chisom <nwisuchisom@gmail.com>
2020-08-14 09:07:32 +0100

    made some ground breaking changes

--- ClientApp/build/service-worker.js
+++ ClientApp/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c22bd6bae3a60f481fa13fd3c3aa1448.js"
+  "/precache-manifest.3df36044ee88d4253b095864af0bd748.js"
 );
 
 self.addEventListener('message', (event) => {
